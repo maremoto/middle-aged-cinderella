74205ede9f59ef982084fd415b189d8ccd3e7e9b
maremoto <mar_martinez@yahoo.com>
2020-05-13 09:28:27 +0200

    Older version uploaded by mistake
    Set the last one instead

--- main.py
+++ main.py
@@ -5,11 +5,14 @@
 import machine
 import time
 
+TEST_MODE = False
+
 # Declare devices
 button = machine.Pin(12, machine.Pin.IN, machine.Pin.PULL_UP)
 pin = machine.Pin(15, machine.Pin.OUT)
 
 # Variables
+global mode, loops_to_run, silent_steps, vibration_steps, step, silent, current_iteration
 mode = 0 # 0=none, 1=mode1, 2=mode2, 3=mode3
 loops_to_run = 0
 current_iteration = 0
@@ -18,36 +21,40 @@ silent_steps = 0
 vibration_steps = 0
 step = 0
 
-# Functions
-def vibrate(duration=1.5):
-    pin.value(1)
-    time.sleep(duration)
-    pin.value(0)
-
-def doDistraction(loops=100):
-    for i in range(loops):
-        vibrate()
-        time.sleep(.8)
-
+# Mode config
 def mode1_params():
-    silent_steps = 20
-    vibration_steps = 80
-    loops_to_run = 600 # 60000
-
-def mode2_params():
-    silent_steps = 50
-    vibration_steps = 50
-    loops_to_run = 600 # 60000
-
-def mode3_params():
+    global silent_steps, vibration_steps, loops_to_run
     silent_steps = 80
     vibration_steps = 20
-    loops_to_run = 600 # 60000
+    if TEST_MODE:
+        loops_to_run = 600
+    else:
+        loops_to_run = 60000
 
+def mode2_params():
+    global silent_steps, vibration_steps, loops_to_run
+    silent_steps = 100
+    vibration_steps = 50
+    if TEST_MODE:
+        loops_to_run = 600
+    else:
+        loops_to_run = 60000
+
+def mode3_params():
+    global silent_steps, vibration_steps, loops_to_run
+    silent_steps = 200
+    vibration_steps = 80
+    if TEST_MODE:
+        loops_to_run = 600
+    else:
+        loops_to_run = 60000
+
+# Helpers
 def switch_mode():
+    global mode, current_iteration, step, silent
     current_iteration = 0
     step = 0
-    silent = 1
+    silent = 0
     if mode == 0:
         mode = 1
         mode1_params()
@@ -59,11 +66,13 @@ def switch_mode():
         mode3_params()
     else:
         mode = 0
+        silent = 1
 
 # Main loop, button surveillance
 while True:
     first = button.value()
     time.sleep(0.01)
+    current_iteration += 1
     step += 1
     second = button.value()
     #if first and not second:
@@ -90,3 +99,5 @@ while True:
             step = 0
             pin.value(0)
 
+    if current_iteration > loops_to_run:
+        mode = 0
